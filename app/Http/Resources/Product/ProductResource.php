29c0cd95206a7d46d148e27e36ca7463de01b745
daspintu96 <pintu2009s@gmail.com>
2021-11-28 17:43:24 +0600

    Review link done

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,10 +18,10 @@ class ProductResource extends JsonResource
             'name'=>$this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
+            'stock'=>$this->stock == 0? "Out Of Stock" : $this->stock,
             'discount'=>$this->discount."%",
             'TotalPrice'=> round((1-($this->discount/100))*$this->price,2),
-            'rating'=> $this->reviews->count() > 0 ? round($this->reviews->count()/$this->reviews->sum('star'),2) : "No raiting Yet",
+            'rating'=> $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : "No raiting Yet",
             'href'=>[
                 'reviews'=>route('review.index',$this->id)
             ]
