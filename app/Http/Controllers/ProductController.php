a713890aba283833d99e61402f1969ee847bab0a
daspintu96 <pintu2009s@gmail.com>
2021-11-29 12:18:07 +0600

    Update are comeplete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,27 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
+use App\Http\Requests\ProductRequest;
 use App\Models\Product;
+
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        // $this->middleware('auth:sanctum')->except('index','show');
+    }
+   
     public function index()
     {
         // return ProductCollection::collection(Product::all());
        return ProductCollection::collection(Product::paginate(10));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function create()
     {
         //
@@ -36,52 +35,44 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+      
+       $product = new Product();
+       $product->name = $request->name;
+       $product->detail = $request->description;
+       $product->price = $request->price;
+       $product->stock = $request->stock;
+       $product->discount = $request->discount;
+       $product->save();
+
+       return response([
+           'data'=> new ProductResource($product)
+       ],Response::HTTP_CREATED);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
+ 
     public function show(Product $product)
     {
         // return $product;
         return new ProductResource($product);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit(Product $product)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(Request $request, Product $product)
     {
-        //
+        $request['detail'] = $request->description;
+        unset($request['description']);
+        return $product->update($request->all());
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy(Product $product)
     {
         //
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,7 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name','detail','price','stock','discount'];
     public function reviews()
     {
         return $this->hasMany('App\Models\Review');
